b7a3bc9634a1f2d48d20fcd23f2039345e61f8ae
Naveen Kumar <naveenmishraknp@gmail.com>
2013-10-15 15:53:08 +0530

    bicycle_mgt_sys

---
+++ bicycle_mgt_sys/home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Home</title>
+<style type="text/css">
+<!--
+.style2 {color: #FFFFFF}
+body {
+	background-color: #F7C76C;
+}
+.style3 {
+	font-size: 24px;
+	color: #0000CC;
+	font-weight: bold;
+}
+.style5 {font-size: 24px; font-weight: bold; }
+-->
+</style></head>
+
+<body>
+<table width="1217" height="110" border="1">
+  <tr>
+    <td width="1207" height="104" bgcolor="#3366FF"><div align="center">
+      <h1><span class="style2">Bi-cycle Management for NIT-T </span></h1>
+    </div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="563" height="358" border="1" align="center">
+  <tr>
+    <td><img src="img.jpg" alt="cycle" width="625" height="500" /></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<table width="156" height="44" border="1">
+  <tr>
+    <td width="291"><div align="center"><span class="style5"><a href="register.php">Register</a></span></div></td>
+  </tr>
+</table>&nbsp;&nbsp;&nbsp;&nbsp;
+   <table width="158" height="46" border="1">
+     <tr>
+       <td align="center"><div align="right" class="style3">
+         <div align="center"><a href="rent.php">Rent</a></div>
+       </div></td>
+     </tr>
+   </table>
+   <p>&nbsp;</p>
+   <blockquote>
+   <blockquote>
+     <p>&nbsp;</p>
+     <p align="center">&nbsp;	</p>
+   </blockquote>
+ </blockquote>
+</body>
+</html>
---
+++ bicycle_mgt_sys/sign_up.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>New Registration</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #F7C76C;
+}
+.style1 {
+	font-size: 36px;
+	color: #FFFFFF;
+}
+.style2 {
+	font-size: 24px;
+	color: #FF0000;
+}
+-->
+</style></head>
+
+<body>
+
+
+<p>&nbsp;</p>
+<table width="1211" height="78" border="1" align="center" bgcolor="#3366FF">
+  <tr>
+    <td><div align="center"><strong><span class="style1">New Registration </span></strong></div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="470" height="415" border="1" align="center">
+  <tr>
+    <td width="237" height="58" bgcolor="#CCCCCC"><div align="center"><strong><span class="style2">Name</span></strong></div></td>
+    <td width="183">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC"><div align="center" class="style2"><strong>Roll No </strong></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC" class="style2"><div align="center"><strong>Model No. </strong></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC" class="style2"><div align="center"><strong>Date</strong></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC" class="style2"><div align="center"><strong>Course</strong></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC" class="style2"><div align="center"><strong>Department</strong></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC" class="style2"><div align="center"><strong>Mobile No </strong></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#CCCCCC" class="style2"><div align="center"><strong>Address</strong></div></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+      
+<p align="center">
+  <input name="Submit" type="submit" class="style2" value="Submit" />
+</p>
+<form id="form1" name="form1" method="post" action="">
+  <label></label>
+</form>
+<p>&nbsp;</p>
+<p align="center">&nbsp;</p>
+<blockquote>
+  <blockquote>
+    <blockquote>
+      <p align="center" class="style1">&nbsp;</p>
+    </blockquote>
+  </blockquote>
+</blockquote>
+</body>
+
+
+</html>
